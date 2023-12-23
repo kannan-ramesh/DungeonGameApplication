74623c477d70b00a7fe53986b5fe8e79f9c7335a
N RAMESH KANNAN <43806145+kannan-ramesh@users.noreply.github.com>
2023-12-23 11:48:08 +0530

    second Case

--- DungeonGameApplication/src/com/kannanrameshrk/Main.java
+++ DungeonGameApplication/src/com/kannanrameshrk/Main.java
@@ -6,32 +6,50 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Main main = new Main();
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter Row:");
-		int row=input.nextInt();
+		int row = input.nextInt();
 		System.out.println("ENter Col:");
-		int col=input.nextInt();
-		
-		startGame(row,col,input);
+		int col = input.nextInt();
+
+		main.startGame(row, col, input);
 	}
 
-	private static void startGame(int row, int col, Scanner input) {
+	private void startGame(int row, int col, Scanner input) {
 		char[][] arr = new char[row][col];
-		
+
 		System.out.println("Position Of adventure:");
-		int addventRow=input.nextInt();
-		int addventCol=input.nextInt();
-		arr[addventRow-1][addventCol-1]='A';
-		
+		int addventRow = input.nextInt();
+		int addventCol = input.nextInt();
+		arr[addventRow - 1][addventCol - 1] = 'A';
+
+		System.out.println("Position Of monster:");
+		int monsterRow = input.nextInt();
+		int monsterCol = input.nextInt();
+		arr[monsterRow - 1][monsterCol - 1] = 'M';
+
 		System.out.println("Position Of Gold:");
-		int goldRow=input.nextInt();
-		int goldCol=input.nextInt();
-		arr[goldRow-1][goldCol-1]='G';
-		
-		int step=Math.abs(addventRow-goldRow) + Math.abs(addventCol-goldCol);
+		int goldRow = input.nextInt();
+		int goldCol = input.nextInt();
+		arr[goldRow - 1][goldCol - 1] = 'G';
+
+		int adventstep = Math.abs(addventRow - goldRow) + Math.abs(addventCol - goldCol);
+		int monsterStep = Math.abs(monsterRow - goldRow) + Math.abs(monsterCol - goldCol);
 		System.out.println(Arrays.deepToString(arr));
-		
-		System.out.println("Min number of steps:"+step);
+//		
+//		System.out.println("Min number of steps:"+adventstep);
+//		System.out.println("monster Step"+monsterStep);
+
+		resultGame(adventstep, monsterStep);
+	}
+
+	private void resultGame(int adventStep, int monsterStep) {
+		if (adventStep > monsterStep) {
+			System.out.println("No Possible Solution..");
+		} else {
+			System.out.println("Minimum Number Of Steps:" + adventStep);
+		}
 	}
 
 }
